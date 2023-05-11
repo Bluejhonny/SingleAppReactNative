df9ad604288bca3f9e8684334bb87b524153a905
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2023-05-10 21:14:57 -0400

    first commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+
+    return (
+        <View> 
+            <Text> Home This is an App Folder</Text>
+        </View>
+    )
+};
+
+export default Home;
